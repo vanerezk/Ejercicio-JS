2528635a23f3b8b55e2d01c99c7c74f99f4c1e06
glorian <glorianarrietacj@gmail.com>
2023-10-28 15:23:16 +0200

    funcion disminuir y fondo de pagina

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-'use strict';
+"use strict";
 
-let contador = document.querySelector("#numero")
+let contador = document.querySelector("#numero");
 
 let valor = contador.value;
 
 function aumentar() {
-    valor++;
-    contador.value = valor;
+  valor++;
+  contador.value = valor;
 }
 
 document.querySelector("#boton_1").addEventListener("click", aumentar);
 
+function disminuir() {
+  valor--;
+  contador.value = valor;
+}
+
+document.querySelector("#boton_2").addEventListener("click", disminuir);
